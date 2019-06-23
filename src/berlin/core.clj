5f8eb6bd4c831a5777b117be1d55b6323c88c600
Luca Grulla <luca.grulla@gmail.com>
2019-06-23 18:33:32 +0100

    refactoring: declare components order explicity in berlin function

--- src/berlin/core.clj
+++ src/berlin/core.clj
@@ -3,38 +3,46 @@
 
 (defmulti transform (fn [type _] type))
 (defmethod transform :hours [_ h]
-  (let [active-hours (quot h 5);; block of 5 hours
-        modulus (mod h 5);; single hours
-        a (s/join (map (fn [v]
+  (let [number-of-fields 4
+        active-hours (quot h 5)
+        modulus (mod h 5)
+        h1 (s/join (map (fn [v]
                          (if (< v active-hours) "R" "O")) 
-                       (range 4)))
-        b (s/join (map (fn [v]
+                       (range number-of-fields)))
+        h2 (s/join (map (fn [v]
                          (if (< v modulus) "R" "O"))
-                       (range 4)))]
-    (s/join  " " [a b])))
+                       (range number-of-fields)))]
+    [h1 h2]))
 
 (defmethod transform :minutes [_ m]
-  (let [active-minutes (quot m 5)
+  (let [number-of-minute-fields 11
+        number-of-minute-fields-2 4
+        active-minutes (quot m 5)
         modulus (mod m 5)
-        a (s/join (map (fn [v]
+        m1 (s/join (map (fn [v]
                          (cond
                            (and (some #{v} [2 5 8]) (< v active-minutes )) "R"
                            (< v active-minutes)                            "Y"
                            :else                                           "O"
                            ))
-                       (range 11)))
-        b (s/join (map (fn [x]
+                       (range number-of-minute-fields)))
+        m2 (s/join (map (fn [x]
                          (if (< x modulus) "Y" "O"))
-                       (range 4)))]
-    (s/join " " [a b])))
+                       (range number-of-minute-fields-2)))]
+    [m1 m2]))
 (defmethod transform :seconds [_ s]
   (if (even? s) "Y" "O"))
 
 (defn berlin [i]
   ;;TODO input validation
-  (let [clock-tokens (->> (s/split i #":")
-                          (map (fn [x] (Integer/parseInt x)))
-                          (zipmap `(:hours :minutes :seconds))
-                          (reduce-kv (fn [acc k v] (assoc acc k (transform k v))) {}))
-        {:keys [:seconds :hours :minutes]} clock-tokens]
-    (s/join " " [seconds hours minutes])))
+  (let [clock-order [:seconds :hours :minutes]
+        clock-tokens (->> (s/split i #":")
+                          (zipmap `(:hours :minutes :seconds)))]
+    (->> clock-order
+         (map (fn [k]
+                (->> k
+                    (get clock-tokens)
+                    ((fn [x] (Integer/parseInt x)))
+                    (transform k))))
+         flatten
+         (s/join " "))))
--- test/berlin/core_test.clj
+++ test/berlin/core_test.clj
@@ -4,17 +4,17 @@
 
 (deftest berlin-clock
   (testing "hours"
-    (is (= (transform :hours 10) "RROO OOOO"))
-    (is (= (transform :hours 5) "ROOO OOOO"))
-    (is (= (transform :hours 13) "RROO RRRO"))
-    (is (= (transform :hours 4) "OOOO RRRR")))
+    (is (= (transform :hours 10) ["RROO" "OOOO"]))
+    (is (= (transform :hours 5) ["ROOO" "OOOO"]))
+    (is (= (transform :hours 13) ["RROO" "RRRO"]))
+    (is (= (transform :hours 4) ["OOOO" "RRRR"])))
   (testing "minutes"
-    (is (= (transform :minutes 7) "YOOOOOOOOOO YYOO"))
-    (is (= (transform :minutes 10) "YYOOOOOOOOO OOOO"))
-    (is (= (transform :minutes 15) "YYROOOOOOOO OOOO"))
-    (is (= (transform :minutes 30) "YYRYYROOOOO OOOO"))
-    (is (= (transform :minutes 45) "YYRYYRYYROO OOOO"))
-    (is (= (transform :minutes 52) "YYRYYRYYRYO YYOO")))
+    (is (= (transform :minutes 7) ["YOOOOOOOOOO" "YYOO"]))
+    (is (= (transform :minutes 10) ["YYOOOOOOOOO" "OOOO"]))
+    (is (= (transform :minutes 15) ["YYROOOOOOOO" "OOOO"]))
+    (is (= (transform :minutes 30) ["YYRYYROOOOO" "OOOO"]))
+    (is (= (transform :minutes 45) ["YYRYYRYYROO" "OOOO"]))
+    (is (= (transform :minutes 52) ["YYRYYRYYRYO" "YYOO"])))
   (testing "seconds"
     (is (= (transform :seconds 10) "Y"))
     (is (= (transform :seconds 0) "Y")))
